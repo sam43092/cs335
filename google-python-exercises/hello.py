e432e3ed37c8d88e310088243fe3f1d0247d522c
Samantha Long <sam43092@ymail.com>
2019-01-15 16:38:05 +0000

    Modified hello.py

--- google-python-exercises/hello.py
+++ google-python-exercises/hello.py
@@ -25,8 +25,8 @@ def main():
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
-    name = 'World'
-  print 'Hello', name
+    name = 'World Samantha'
+  print 'Hello', name, 'Samantha'
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
