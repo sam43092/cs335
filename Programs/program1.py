bfbf304b801261346e12f8b94d2ea6c68182e5fa
Samantha Long <sam43092@ymail.com>
2019-01-17 16:46:28 +0000

    Daily 1/17/19

--- Programs/program1.py
+++ Programs/program1.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+
 import sys
 
 def mult(x,y):
-    print(x*y)
+    print(str(x*y))
+
+if __name__ == '__main__':
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    mult(num1,num2)
     
-    
-mult(3,5)
-mult(10,9)
-    
-    
+  
